9e5609b74a64be7766415e5f9c6307db100cbee9
Andrew Halle <ahalle@berkeley.edu>
2021-05-24 20:11:32 -0700

    Start to figure out the data layout
    * add types for rows, columns, tables, etc.
    * define display traits for some items so I can print out all rows
    * sketch out query / execute methods, stub with fetch all rows in first
      table

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,128 @@
+use std::fmt::{Display, Formatter};
 use std::io::{self, BufRead, Write};
 
-#[derive(Debug)]
-struct User {
-    id: String,
-    email: String,
-    first_name: String,
-    last_name: String,
-    age: u32,
+struct Database {
+    tables: Vec<Table>,
 }
 
-struct Database(Vec<User>);
+struct Table {
+    name: String,
+    columns: Vec<Column>,
+    rows: Vec<Row>,
+}
+
+#[derive(Clone)]
+struct Column {
+    name: String,
+    datatype: Datatype,
+}
+
+// TODO this and Value are very similar
+#[derive(Clone)]
+enum Datatype {
+    Number,
+    Text,
+}
+
+// TODO this and Datatype are very similar
+#[derive(Clone)]
+enum Value {
+    Number(u32),
+    Text(String),
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        match self {
+            Value::Number(n) => write!(f, "{}", n),
+            Value::Text(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Row(Vec<Value>);
+
+struct Query;
+
+impl Query {
+    // TODO actually parse SQL
+    fn from(source: &str) -> Self {
+        Query
+    }
+}
+
+struct QueryResult {
+    columns: Vec<Column>,
+    rows: Vec<Row>,
+}
+
+impl Display for QueryResult {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let column_names: Vec<&str> = self.columns.iter().map(|c| c.name.as_str()).collect();
+        writeln!(f, "{}", &column_names.join(",")).unwrap();
+
+        let num_rows = self.rows.len();
+        for (i, row) in self.rows.iter().enumerate() {
+            let values: Vec<String> = row.0.iter().map(|v| v.to_string()).collect();
+            write!(f, "{}", &values.join(",")).unwrap();
+            if i != num_rows - 1 {
+                writeln!(f).unwrap();
+            }
+        }
+
+        Ok(())
+    }
+}
 
 impl Database {
+    fn new() -> Self {
+        Database { tables: Vec::new() }
+    }
+
+    // TODO make this generic over a Rust struct implementing something like a IntoTable trait
+    // currently adds a fixed user table with a fixed row
+    fn add_table(&mut self) {
+        let user1 = vec![
+            Value::Text("1".to_string()),
+            Value::Text("user1@email.com".to_string()),
+            Value::Number(25),
+        ];
+        let user2 = vec![
+            Value::Text("2".to_string()),
+            Value::Text("user2@email.com".to_string()),
+            Value::Number(27),
+        ];
+
+        self.tables.push(Table {
+            name: "users".to_string(),
+            rows: vec![Row(user1), Row(user2)],
+            columns: vec![
+                Column {
+                    name: "id".to_string(),
+                    datatype: Datatype::Text,
+                },
+                Column {
+                    name: "email".to_string(),
+                    datatype: Datatype::Text,
+                },
+                Column {
+                    name: "age".to_string(),
+                    datatype: Datatype::Number,
+                },
+            ],
+        });
+    }
+
+    fn execute(&mut self, query: Query) -> QueryResult {
+        let table = &self.tables[0];
+
+        QueryResult {
+            columns: table.columns.clone(),
+            rows: table.rows.clone(),
+        }
+    }
+
     fn console(&mut self) {
         let stdin = io::stdin();
         let mut stdout = io::stdout();
@@ -29,43 +140,22 @@ impl Database {
                 break;
             }
 
-            print!("{}", line);
+            let query = Query::from(&line);
+            let result = self.execute(query);
+            println!("{}", result);
         }
     }
 }
 
-fn main() {
-    let users = vec![
-        User {
-            id: "1".to_string(),
-            email: "user1@test.com".to_string(),
-            first_name: "User1".to_string(),
-            last_name: "Person".to_string(),
-            age: 14,
-        },
-        User {
-            id: "2".to_string(),
-            email: "user2@test.com".to_string(),
-            first_name: "User2".to_string(),
-            last_name: "Person".to_string(),
-            age: 24,
-        },
-        User {
-            id: "3".to_string(),
-            email: "user3@test.com".to_string(),
-            first_name: "User3".to_string(),
-            last_name: "Person".to_string(),
-            age: 34,
-        },
-        User {
-            id: "4".to_string(),
-            email: "user4@test.com".to_string(),
-            first_name: "User4".to_string(),
-            last_name: "Person".to_string(),
-            age: 44,
-        },
-    ];
+#[derive(Debug)]
+struct User {
+    id: String,
+    email: String,
+    age: u32,
+}
 
-    let mut db = Database(users);
+fn main() {
+    let mut db = Database::new();
+    db.add_table();
     db.console();
 }
