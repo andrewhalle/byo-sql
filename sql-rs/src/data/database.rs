c71421073853ad1057463ceb94be5a574129f17a
Andrew Halle <ahalle@berkeley.edu>
2021-07-15 20:21:08 -0700

    Move bin only functions to main.rs
    * move Database::console and Database::seed to main.rs

--- sql-rs/src/data/database.rs
+++ sql-rs/src/data/database.rs
@@ -1,16 +1,9 @@
 use std::collections::HashMap;
-use std::fs;
-use std::io::{self, BufRead, Write};
-use std::path::PathBuf;
-use std::process;
-
-use ansi_term::Colour;
 
 use super::Table;
 use crate::execute::{QueryResult, SelectQueryResult, SelectQueryResultColumn};
 use crate::new_alias_map;
 use crate::parse::ast;
-use crate::parse::parse_queries;
 use crate::Query;
 use crate::Value;
 
@@ -85,61 +78,6 @@ impl Database {
             }
             _ => unimplemented!(),
         }
-    }
-
-    pub fn console(&mut self) {
-        let stdin = io::stdin();
-        let mut stdout = io::stdout();
-        let mut reader = stdin.lock();
-        let mut line = String::new();
-
-        loop {
-            line.clear();
-            print!("> ");
-            stdout.flush().unwrap();
-            reader.read_line(&mut line).unwrap();
-
-            if line == "" {
-                println!();
-                break;
-            }
-
-            let queries = parse_queries(&line);
-
-            match queries {
-                Ok(queries) => {
-                    for query in queries.0 {
-                        match self.execute(query) {
-                            Ok(s) => println!("{}", s),
-                            Err(e) => println!("{}", e),
-                        }
-                    }
-                }
-                Err(parse_error) => {
-                    println!("{}", parse_error);
-                }
-            }
-        }
-    }
-
-    pub fn seed(&mut self, seed_file: PathBuf) {
-        let seed = fs::read_to_string(seed_file).unwrap();
-        let queries = parse_queries(&seed);
-
-        match queries {
-            Ok(queries) => {
-                for query in queries.0 {
-                    self.execute(query).unwrap();
-                }
-            }
-            Err(parse_error) => {
-                println!("{}", parse_error);
-                process::exit(1);
-            }
-        }
-
-        let style = Colour::Fixed(251).italic();
-        println!("{}\n\n{}", style.paint("Seeded with:"), style.paint(&seed));
     }
 }
 
--- sql-rs/src/main.rs
+++ sql-rs/src/main.rs
@@ -1,8 +1,14 @@
+use std::fs;
+use std::io::{self, BufRead, Write};
 use std::path::PathBuf;
+use std::process;
 
 use structopt::StructOpt;
 
+use ansi_term::Colour;
+
 use sql_rs::data::Database;
+use sql_rs::parse::parse_queries;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -10,13 +16,68 @@ struct Opt {
     seed_file: Option<PathBuf>,
 }
 
+pub fn seed(database: &mut Database, seed_file: PathBuf) {
+    let seed = fs::read_to_string(seed_file).unwrap();
+    let queries = parse_queries(&seed);
+
+    match queries {
+        Ok(queries) => {
+            for query in queries.0 {
+                database.execute(query).unwrap();
+            }
+        }
+        Err(parse_error) => {
+            println!("{}", parse_error);
+            process::exit(1);
+        }
+    }
+
+    let style = Colour::Fixed(251).italic();
+    println!("{}\n\n{}", style.paint("Seeded with:"), style.paint(&seed));
+}
+
+pub fn console(database: &mut Database) {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let mut reader = stdin.lock();
+    let mut line = String::new();
+
+    loop {
+        line.clear();
+        print!("> ");
+        stdout.flush().unwrap();
+        reader.read_line(&mut line).unwrap();
+
+        if line == "" {
+            println!();
+            break;
+        }
+
+        let queries = parse_queries(&line);
+
+        match queries {
+            Ok(queries) => {
+                for query in queries.0 {
+                    match database.execute(query) {
+                        Ok(s) => println!("{}", s),
+                        Err(e) => println!("{}", e),
+                    }
+                }
+            }
+            Err(parse_error) => {
+                println!("{}", parse_error);
+            }
+        }
+    }
+}
+
 fn main() {
     let opt = Opt::from_args();
 
     let mut db = Database::new();
     if let Some(seed_file) = opt.seed_file {
-        db.seed(seed_file);
+        seed(&mut db, seed_file);
     }
 
-    db.console();
+    console(&mut db);
 }
